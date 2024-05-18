b9716d57fd3ff08ddc9b7b47f45f42fe20bbd1b6
Micheluzzooo <156191514+Micheluzzooo@users.noreply.github.com>
2024-05-18 17:31:56 +0200

    Update Role.java

--- src/main/java/com/develhope/spring/entity/enums/Role.java
+++ src/main/java/com/develhope/spring/entity/enums/Role.java
@@ -3,6 +3,16 @@ package com.develhope.spring.entity.enums;
 public enum Role {
     AMMINISTRATORE,
     ACQUIRENTE,
-    VENDITORE;
+    VENDITORE,
+    NON_DEFINITO;
+
+    public static Role convertStringToRole(String role) {
+        return switch (role.toLowerCase()) {
+            case "amministratore" -> Role.AMMINISTRATORE;
+            case "acquirente" -> Role.ACQUIRENTE;
+            case "venditore" -> Role.VENDITORE;
+            default -> Role.NON_DEFINITO;
+        };
+    }
 
 }
