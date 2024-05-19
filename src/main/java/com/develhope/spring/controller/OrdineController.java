c02c06b939cd9dac6984faa53948d53dfab75db4
Lutti Davide <luttidavide004@gmail.com>
2024-05-19 21:02:49 +0200

    creato solo la classe OrdineController

--- src/main/java/com/develhope/spring/controller/OrdineController.java
+++ src/main/java/com/develhope/spring/controller/OrdineController.java
@@ -1,4 +1,12 @@
 package com.develhope.spring.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ordine")
 public class OrdineController {
+
+
+
 }
